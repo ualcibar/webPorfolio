9997fd115cb0df47ccd3f6d7a6701379946c2ac6
Unai Alcibar <ualcibar11@gmail.com>
2026-09-03 13:13:58 +0200

    added font and color pallete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,24 @@
 import { ReactNode } from 'react';
 import './ui/global.css';
- 
+import localFont from 'next/font/local';
+
+const adventor = localFont({
+  src: [
+    { path: './fonts/tex-gyre-adventor.regular.otf', weight: '400', style: 'normal' },
+    { path: './fonts/tex-gyre-adventor.italic.otf', weight: '400', style: 'italic' },
+    { path: './fonts/tex-gyre-adventor.bold.otf', weight: '700', style: 'normal' },
+    { path: './fonts/tex-gyre-adventor.bold-italic.otf', weight: '700', style: 'italic' },
+  ],
+  variable: '--font-adventor',
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={adventor.variable}>
       <body>{children}</body>
     </html>
   );
