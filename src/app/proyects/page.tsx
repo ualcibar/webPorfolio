4772d6e46d3f98a4c77dd0d9c48783f236cf816a
Unai Alcibar <ualcibar11@gmail.com>
2026-09-09 16:56:03 +0200

    adjst projetcs page

--- src/app/proyects/page.tsx
+++ src/app/proyects/page.tsx
@@ -4,8 +4,8 @@
 
 export default function ProyectsPage() {
   return (
-      <div className ="bg-photo1-bg h-full flex flex-col items-center justify-center gap-4 p-4">
-        <div className="inline-grid grid-cols-3 gap-4 h-170 w-300 pb-10">
+      <div className ="bg-photo1-bg h-full flex flex-col items-center justify-center gap-4">
+        <div className="inline-grid grid-cols-3 gap-4 h-170 w-250 mb-12 pb-10">
             <div className="bg-white" >01</div>
             <div className="bg-white">02</div>
             <div className="bg-white">03</div>
