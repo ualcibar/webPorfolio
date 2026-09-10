d64bf7c155e732078ed82b856c0f45de07ae8626
Unai Alcibar <ualcibar11@gmail.com>
2026-09-10 18:28:25 +0200

    style correction

--- src/app/proyects/[proyect]/page.tsx
+++ src/app/proyects/[proyect]/page.tsx
@@ -25,12 +25,11 @@ export default async function ProyectPage({ params }: ProyectPageProps) {
   }
   return (
     <div className="bg-photo1-bg h-full flex flex-col items-center justify-center gap-4 p-4">
-      <div className="grid grid-cols-[25rem_1fr] gap-4 w-275 h-152 ml-45 mb-12 pb-10">
+      <div className="grid grid-cols-[25rem_1fr] gap-9 w-275 h-152 ml-15 mb-12 pb-10">
         <div className="h-full">
-          <div className="p-8 p-r4">
+          <div className="p-8 pr-4">
             <h1 className="text-3xl font-bold pb-6">{proyect.title}</h1>
             <p>{proyect.description}</p>
-            
           </div>
         </div>
         <PhotoGalery proyect={proyect}/>
